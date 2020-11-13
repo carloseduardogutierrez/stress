c8c41cf616c8966ac40ed14bf206b6bd3e1c559e
carloseduardogutierrez <carlos.gutierrez@globant.com>
2020-11-13 13:44:19 -0300

    modify test controller

--- src/main/java/com/stress/stress/controller/TestController.java
+++ src/main/java/com/stress/stress/controller/TestController.java
@@ -8,27 +8,25 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
+@RequestMapping(value = "/v1/pmc_services/core_services")
 public class TestController {
 
-    //no esta disponible
-    @PostMapping("/v1/pmc_services/core_services/debts_details")
+    @PostMapping("/debts_details")
     Object getDebtsDetails() {
         Resource resource = new ClassPathResource("/static/json/debtsDetails.json");
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
+            return getObject(resource);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    @PostMapping("/v1/pmc_services/core_services/{variable1}/categories")
+    @GetMapping("/{pathParam}/categories")
     Object getCategories() {
         Resource resource = new ClassPathResource("/static/json/categories.json");
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
+            return getObject(resource);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,77 +34,47 @@ public class TestController {
     }
 
     //no esta disponible
-    @PostMapping("/v1/pmc_services/core_services/subscriptions1")
+    @PostMapping("/subscriptions")
     Object addAdhesion() {
-        Resource resource = new ClassPathResource("/static/json/addAdhesion.json");
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return true;
     }
 
-    //no esta disponible
-    @PostMapping("/v1/pmc_services/core_services/subscriptions2")
-    Object modifyAdhesion() {
-        Resource resource = new ClassPathResource("/static/json/modifyAdhesion.json");
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    //no esta disponible
-    @PostMapping("/v1/pmc_services/core_services/subscriptions3")
-    Object deleteAdhesion() {
-        Resource resource = new ClassPathResource("/static/json/deleteAdhesion.json");
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @PostMapping("/v1/pmc_services/core_services/{variable1}/categories/{variable2}}/companies/{variable3}")
+    @GetMapping("/{pathParam1}/categories/{pathParam2}/companies/{pathParam3}")
     Object getCompanies() {
         Resource resource = new ClassPathResource("/static/json/oneCompanies.json");
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
+            return getObject(resource);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    @PostMapping("/v1/pmc_services/core_services/{variable1}/companies")
+    @GetMapping("/{pathParam1}/companies")
     Object getAllCompanies() {
         Resource resource = new ClassPathResource("/static/json/allCompanies.json");
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
+            return getObject(resource);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    @PostMapping("/v1/pmc_services/core_services/{variable1}/categories/{variable2}/companies")
+    @GetMapping("/{pathParam1}/categories/{pathParam2}/companies")
     Object getAllCompaniesCategory() {
         Resource resource = new ClassPathResource("/static/json/allCompaniesCategory.json");
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(resource.getInputStream(), Object.class);
+            return getObject(resource);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private Object getObject(Resource resource) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(resource.getInputStream(), Object.class);
     }
 
 }
